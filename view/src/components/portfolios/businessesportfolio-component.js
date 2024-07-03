f4dae4c4a96b2152314ab5900d2ba1c84e7b392d
markie311 <markie311@gmail.com>
2024-07-03 13:09:44 +0800

    Vanguard

--- view/src/components/portfolios/businessesportfolio-component.js
+++ view/src/components/portfolios/businessesportfolio-component.js
@@ -12,68 +12,68 @@ export default function BusinessesPortfolio(props) {
    <Row id="businessesportfolio">
        <p className="businessesportfolio-headerindication">Businesses and their Portfolio's</p>
    
-     <Row id="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer">
-        {
-        props.businesses.map((business, businessidx)=> {
-          return (
-              <Col xs={12}
-                md={4}
-                lg={4}
-                className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer">
-              <Col className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-layoutcontainer">
-              <p className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyheaderindication">{business.details.businessname}</p>
-              {
-              business.product.list.map((businessproduct, businessproductidx)=> {
-              return (
-                <Col className="businessesportfolioxirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistscontainer"
-                      key={businessproductidx}>
+    <Row id="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer">
+      {
+      props.businesses.map((business, businessidx)=> {
+        return (
+            <Col xs={12}
+              md={4}
+              lg={4}
+              className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer">
+            <Col className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-layoutcontainer">
+            <p className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyheaderindication">{business.details.businessname}</p>
+            {
+            business.product.list.map((businessproduct, businessproductidx)=> {
+            return (
+              <Col className="businessesportfolioxirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistscontainer"
+                    key={businessproductidx}>
 
-                    <p className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistsheaderindication">{businessproduct.productname}'s, {businessproduct.productname}</p>
-                    <p className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistsheaderindication">Made 0 peso's as profit and revenue on all marketed product's</p>
-                    <p className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistsheaderindication">Kukaku also gave contributions to commoner's, to the public and private authentication's through Vanguards transactions giving the VAT as it's self profit and revenue.</p>
-                    <p className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistsheaderindication">{business.details.businessname} contributed to Vanguard, <button className="xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistsheaderindication-seeprojectsbutton"
-                    onClick={()=> {
-                      const _businessprojectscontainer = document.querySelectorAll(".businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-companyprojectcontainer");
-                      _businessprojectscontainer[0].style.display = "block";
-                      }}>see project's</button></p>
-                    <p className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistsheaderindication">{businessproduct.productname}'s available product's depending on variations</p>
+                  <p className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistsheaderindication">{businessproduct.productname}'s, {businessproduct.productname}</p>
+                  <p className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistsheaderindication">Made 0 peso's as profit and revenue on all marketed product's</p>
+                  <p className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistsheaderindication">Kukaku also gave contributions to commoner's, to the public and private authentication's through Vanguards transactions giving the VAT as it's self profit and revenue.</p>
+                  <p className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistsheaderindication">{business.details.businessname} contributed to Vanguard, <button className="xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistsheaderindication-seeprojectsbutton"
+                  onClick={()=> {
+                    const _businessprojectscontainer = document.querySelectorAll(".businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-companyprojectcontainer");
+                    _businessprojectscontainer[0].style.display = "block";
+                    }}>see project's</button></p>
+                  <p className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistsheaderindication">{businessproduct.productname}'s available product's depending on variations</p>
 
-                    <Col className="businessesportfolioxirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistscontainer-productspecificationcontainer">
-                    {
-                    businessproduct.specifications.map((_businessproductspecification, _businessproductspecificationidx)=> {
-                      return (
-                          <Col className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistscontainer-productspecificationcontainer-productspecification">
-                          <p className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistsheaderindication">0{_businessproductspecificationidx}. {_businessproductspecification.productname}, made 0 peso's as profit and revenue on all marketed product's</p>
-                          <Col className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistscontainer-productspecificationcontainer-productspecification-buttonscontainer"> 
-                            <button className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistscontainer-productspecificationcontainer-productspecification-buttonscontainer-viewbutton"
-                                      onClick={()=> { 
-                                      const _productdetailscontainer = document.querySelectorAll(".businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-productdetailscontainer");
-                                      _productdetailscontainer[0].style.display = "block";
-                                  }}>view product</button>
-                            <button className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistscontainer-productspecificationcontainer-productspecification-buttonscontainer-graphbutton"
-                                      onClick={()=> {
-                                      const _productgraphcontainer = document.querySelectorAll(".businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-productgraphcontainer");
-                                      _productgraphcontainer[0].style.display = "block";
-                                      }}>
-                                graph
-                            </button>
-                          </Col>
-                          </Col>
-                      )
-                    })
-                    }
-                    </Col>
+                  <Col className="businessesportfolioxirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistscontainer-productspecificationcontainer">
+                  {
+                  businessproduct.specifications.map((_businessproductspecification, _businessproductspecificationidx)=> {
+                    return (
+                        <Col className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistscontainer-productspecificationcontainer-productspecification">
+                        <p className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistsheaderindication">0{_businessproductspecificationidx}. {_businessproductspecification.productname}, made 0 peso's as profit and revenue on all marketed product's</p>
+                        <Col className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistscontainer-productspecificationcontainer-productspecification-buttonscontainer"> 
+                          <button className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistscontainer-productspecificationcontainer-productspecification-buttonscontainer-viewbutton"
+                                    onClick={()=> { 
+                                    const _productdetailscontainer = document.querySelectorAll(".businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-productdetailscontainer");
+                                    _productdetailscontainer[0].style.display = "block";
+                                }}>view product</button>
+                          <button className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-detailsgridcontainer-colcontainer-companyproductslistscontainer-productspecificationcontainer-productspecification-buttonscontainer-graphbutton"
+                                    onClick={()=> {
+                                    const _productgraphcontainer = document.querySelectorAll(".businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-productgraphcontainer");
+                                    _productgraphcontainer[0].style.display = "block";
+                                    }}>
+                              graph
+                          </button>
+                        </Col>
+                        </Col>
+                    )
+                  })
+                  }
+                  </Col>
 
-                </Col>
-              )
-              })
-              }
               </Col>
-              </Col>
-          )
-        })
-        }
-     </Row>
+            )
+            })
+            }
+            </Col>
+            </Col>
+        )
+      })
+      }
+    </Row>
 
     <Row className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-productgraphcontainer">
         <Col xs={12}
@@ -267,6 +267,21 @@ export default function BusinessesPortfolio(props) {
       </p>
       </Col>
     </Col>
+
+    <Col className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-productdetailscontainer">
+        <Col className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-productdetailscontainer-productdetailscontainer">
+        </Col>
+        <Col className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-productdetailscontainer-closebuttoncontainer">
+          <p className="businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-productdetailscontainer-closebuttoncontainer-closebuttonheaderindication"
+            onClick={()=> {
+              const _productdetailscontainer = document.querySelectorAll(".businessesportfolio-xirbitmarketingnavigationbar-marketdetailscontainer-productdetailscontainer");
+              _productdetailscontainer[0].style.display = "none";
+            }}>
+            close
+          </p>
+        </Col>
+      </Col>
+
 
    </Row>
  )
